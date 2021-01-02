da2acceccd1426ea3be4f082fb08be5c2c53a9ae
Nilesh Patel <palnil@gmail.com>
2021-01-01 22:12:55 -0500

    fixes maven dependency for test coverage

--- src/test/java/com/yoshallc/spring/reactive/BaseTestClass.java
+++ src/test/java/com/yoshallc/spring/reactive/BaseTestClass.java
@@ -11,14 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@DirtiesContext
 @AutoConfigureMessageVerifier
+@DirtiesContext
 public class BaseTestClass {
 
     @Autowired
